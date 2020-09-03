97864b982b677e69f27d7fab50202d86e0fcd560
ms28 ghost134 <ghost.134@hotmail.com>
2020-09-03 19:54:20 +0300

    Удаление лишних файлов
    Ранее проводилась попытка добавления базы данных материалов
    В связи с неудачей наработки было решено удалить из проекта

--- Calc/MathEngrave.cs
+++ Calc/MathEngrave.cs
@@ -9,8 +9,8 @@ namespace Calc
     class MathEngrave
     {
         public float timeResult;
-        float engraveHeadSpeed = 400; //CNC optional parameter
-        float verticalOffset = 0.1f; //Head vertical offset of laser engrave CNC
+        float engraveHeadSpeed = 400; // CNC optional parameter  || Скорость движения головоки ЧПУ станка в мм/с
+        float verticalOffset = 0.1f; // Head vertical offset of laser engrave CNC || Вертикальное смещение головки 0.1мм
         public string length { get; set; }
         public string height { get; set; }
 
@@ -22,9 +22,9 @@ namespace Calc
             timeResult /= 60;
             timeResult *= 2;
 
-            //Alternative math operation all in one
+            // Alternative math operation all in one || Альтернативный вариант арифметического расчета
 
-            //timeResult = ((Convert.ToSingle(height) / ((engraveHeadSpeed / Convert.ToSingle(length)) * verticalOffset)) / 60) * 2;
+            // timeResult = ((Convert.ToSingle(height) / ((engraveHeadSpeed / Convert.ToSingle(length)) * verticalOffset)) / 60) * 2;
         }
     }
 }
